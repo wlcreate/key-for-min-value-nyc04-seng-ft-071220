71b033d7b39c869a5c140609e4798937370a43d2
Waverley Leung <waverley.nyc@gmail.com>
2020-07-21 20:54:11 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest_key = nil
+  smallest_value = nil
+  name_hash.each do |key,value|
+    if smallest_value == nil || value < smallest_value
+      smallest_value = value
+      smallest_key = key
+    end
+  end
+  smallest_key
 end
